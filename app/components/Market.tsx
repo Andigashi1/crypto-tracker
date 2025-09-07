6766772de7b8f02fd85c91d78914fb29d8db6a83
Andi Gashi <andig2413@gmail.com>
2025-09-07 16:14:30 +0200

    Added pagination

--- app/components/Market.tsx
+++ app/components/Market.tsx
@@ -1,5 +1,5 @@
 import Coin from "./Coin";
-import MiniChart from "./MiniChart";
+import Pagination from "./Pagination";
 
 export type Coin = {
   id: string;
@@ -16,25 +16,24 @@ export type Coin = {
   };
 };
 
-const Market = async () => {
+const Market = async ({page}: {page: number}) => {
   const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true"
-,
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${page}&sparkline=true`,
     {
       headers: {
         accept: "application/json",
         "x-cg-demo-api-key": "CG-moSedeMvAokQaSbgGW5Py59Z",
       },
+      next: { revalidate: 300 },
     }
   );
 
   const coins: Coin[] = await res.json();
   return (
-    <div className="px-8">
-      <h1>All Coins</h1>
+    <div className="px-8 mt-20 mb-10">
       <table className="w-full table-fixed">
         <thead className="">
-          <tr className="text-left w-full">
+          <tr className="text-left w-full text-gray-500">
             <th className="">Name</th>
             <th className="max-lg:text-right">Price</th>
             <th className="max-lg:hidden">Change 24h</th>
@@ -50,6 +49,7 @@ const Market = async () => {
           ))}
         </tbody>
       </table>
+      <Pagination currentPage={page}/>
     </div>
   );
 };
