04b3b34926d6fd381e73d2355055464705ba581b
Andi Gashi <andig2413@gmail.com>
2025-09-17 10:34:05 +0200

    Separated formatting logic and added Links

--- app/components/Coin.tsx
+++ app/components/Coin.tsx
@@ -1,47 +1,31 @@
 import { Coin as CoinType } from "./Market";
 import MiniChart from "./MiniChart";
+import Link from "next/link";
+import { formatPrice, formatMarketCap, formatPercentage } from "../lib/utils";
 
 type CoinProps = {
   coin: CoinType;
 };
 
-const formatPrice = (value: number) =>
-  new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(value);
-
-  const formatMarketCap = (value: number) => {
-  if (value >= 1_000_000_000_000) {
-    return `$${(value / 1_000_000_000_000).toFixed(2)}T`;
-  } else if (value >= 1_000_000_000) {
-    return `$${(value / 1_000_000_000).toFixed(2)}B`;
-  } else if (value >= 1_000_000) {
-    return `$${(value / 1_000_000).toFixed(2)}M`;
-  } else if (value >= 1_000) {
-    return `$${(value / 1_000).toFixed(2)}K`;
-  }
-  return `$${value}`;
-};
-
-const formatPercentage = (value: number) => `${value.toFixed(2)}%`;
 
 const Coin = ({ coin }: CoinProps) => {
-
   return (
-    <tr className="hover:bg-white/10">
-      <td className="flex items-center gap-4 py-2 lg:pl-2">
-        <img src={coin.image} alt={coin.name} width={32} />
-        <div>
-          <p>{coin.name}</p>
-          <p className="uppercase">{coin.symbol}</p>
-        </div>
+    <tr className="hover:bg-white/10 cursor-pointer">
+      <td className="flex items-center gap-4 py-2">
+        <Link href={`/${coin.id}`} className="flex items-center gap-4">
+          <img src={coin.image} alt={coin.name} width={32} />
+          <div>
+            <p>{coin.name}</p>
+            <p className="uppercase">{coin.symbol}</p>
+          </div>
+        </Link>
       </td>
+
       <td className="max-lg:text-right">
         <div className="max-lg:flex flex-col items-end">
-          <span>{formatPrice(coin.current_price)}</span>
+          <Link href={`/${coin.id}`}>
+            <span>{formatPrice(coin.current_price)}</span>
+          </Link>
           <span
             className={`text-sm ${
               coin.price_change_percentage_24h >= 0
@@ -49,10 +33,11 @@ const Coin = ({ coin }: CoinProps) => {
                 : "text-red-500"
             } lg:hidden`}
           >
-            {coin.price_change_percentage_24h}%
+            {formatPercentage(coin.price_change_percentage_24h)}
           </span>
         </div>
       </td>
+
       <td
         className={`${
           coin.price_change_percentage_24h >= 0
@@ -60,14 +45,26 @@ const Coin = ({ coin }: CoinProps) => {
             : "text-red-500"
         } max-lg:hidden`}
       >
-        {coin.price_change_percentage_24h}%
+        {formatPercentage(coin.price_change_percentage_24h)}
       </td>
+
       <td className="max-lg:hidden">
-        <MiniChart data={coin.sparkline_in_7d?.price || []} />
+        <Link href={`/markets/${coin.id}`}>
+          <MiniChart data={coin.sparkline_in_7d?.price || []} />
+        </Link>
       </td>
-      <td className="max-lg:hidden">{formatMarketCap(coin.market_cap)}</td>
-      <td className="max-lg:hidden">{formatPrice(coin.ath)}</td>
-      <td className="text-right pr-2 max-lg:hidden">{coin.market_cap_rank}</td>
+
+      <td className="max-lg:hidden">
+        <Link href={`/markets/${coin.id}`}>{formatMarketCap(coin.market_cap)}</Link>
+      </td>
+
+      <td className="max-lg:hidden">
+        <Link href={`/markets/${coin.id}`}>{formatPrice(coin.ath)}</Link>
+      </td>
+
+      <td className="text-right pr-2 max-lg:hidden">
+        <Link href={`/markets/${coin.id}`}>{coin.market_cap_rank}</Link>
+      </td>
     </tr>
   );
 };
